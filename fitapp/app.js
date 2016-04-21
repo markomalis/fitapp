f40b92195403140f285c3e18b9ad7bc96f1e51aa
MM <m@m.com>
2016-04-21 12:10:35 +0200

    MM: gitignore added first routes added

--- fitapp/app.js
+++ fitapp/app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var exercise = require('./routes/exercise');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var workout = require('./routes/workout');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/exercise', routes);
 app.use('/users', users);
+app.use('/workout', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
